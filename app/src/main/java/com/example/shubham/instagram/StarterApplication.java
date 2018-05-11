8f7c05a2a50e719c7d64e95725e1060c7a6ca56d
Shubham Agarwal <shubhamagarwal3010@gmail.com>
2018-05-11 11:24:20 +0530

    first commit

---
+++ app/src/main/java/com/example/shubham/instagram/StarterApplication.java
@@ -0,0 +1,48 @@
+package com.example.shubham.instagram;
+/*
+ * Copyright (c) 2015-present, Parse, LLC.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
+
+import android.app.Application;
+import android.util.Log;
+
+import com.parse.Parse;
+import com.parse.ParseACL;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
+
+
+public class StarterApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // Enable Local Datastore.
+        Parse.enableLocalDatastore(this);
+
+        // Add your initialization code here
+        Parse.initialize(new Parse.Configuration.Builder(getApplicationContext())
+                .applicationId("c99dfa4b6575c45ca6e3c284695258a02adf5141")
+                .clientKey("e05551c356bc9e69672ce41f029ad90eabc6c3c4")
+                .server("http://ec2-13-232-18-77.ap-south-1.compute.amazonaws.com/parse")
+                .build()
+        );
+
+
+        ParseUser.enableAutomaticUser();
+
+        ParseACL defaultACL = new ParseACL();
+        defaultACL.setPublicReadAccess(true);
+        defaultACL.setPublicWriteAccess(true);
+        ParseACL.setDefaultACL(defaultACL, true);
+
+    }
+}
